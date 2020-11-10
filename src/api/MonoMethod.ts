d27610ce08ae30858799a19025e1e172e5432934
FreehuntX <freehuntx@web.de>
2020-11-10 15:39:39 +0100

    Added canAccessMethod

--- src/api/MonoMethod.ts
+++ src/api/MonoMethod.ts
@@ -3,6 +3,7 @@ import { MonoClassField } from './MonoClassField'
 import { createNativeFunction } from '../core/native'
 
 export const mono_method_can_access_field = createNativeFunction('mono_method_can_access_field', 'bool', ['pointer', 'pointer'])
+export const mono_method_can_access_method = createNativeFunction('mono_method_can_access_method', 'bool', ['pointer', 'pointer'])
 
 export class MonoMethod extends MonoBase {
   /**
@@ -12,5 +13,14 @@ export class MonoMethod extends MonoBase {
    */
   canAccessField(field: MonoClassField): boolean {
     return mono_method_can_access_field(this.$address, field.$address)
+  }
+
+  /**
+   * Used to determine if the method is allowed to access the specified called method.
+   * @param {MonoMethod} called - The method that we want to probe for accessibility.
+   * @returns {boolean} TRUE if the given method is allowed to invoke the called while following the accessibility rules of the CLI.
+   */
+  canAccessMethod(called: MonoMethod): boolean {
+    return mono_method_can_access_method(this.$address, called.$address)
   }
 }
