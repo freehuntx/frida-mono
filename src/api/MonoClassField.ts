dce4aca4643fc5f2daac26ec1be9092b360af285
FreehuntX <freehuntx@web.de>
2020-11-10 12:20:23 +0100

    Add fullName to MonoClassField

--- src/api/MonoClassField.ts
+++ src/api/MonoClassField.ts
@@ -3,6 +3,7 @@ import { createNativeFunction } from '../core/native'
 
 export const mono_field_get_data = createNativeFunction('mono_field_get_data', 'pointer', ['pointer'])
 export const mono_field_get_offset = createNativeFunction('mono_field_get_offset', 'uint32', ['pointer'])
+export const mono_field_full_name = createNativeFunction('mono_field_full_name', 'pointer', ['pointer'])
 
 export class MonoClassField extends MonoBase {
   /**
@@ -10,6 +11,13 @@ export class MonoClassField extends MonoBase {
    */
   get data(): string {
     const address = mono_field_get_data(this.$address)
+    return address.readUtf8String()
+  }
+  /**
+   * @returns {string} The full name for the field, made up of the namespace, type name and the field name.
+   */
+  get fullName(): string {
+    const address = mono_field_full_name(this.$address)
     return address.readUtf8String()
   }
 
